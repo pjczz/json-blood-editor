883bdab108a6e9bbc110828e96110465b310b286
pjczz <1435211553@qq.com>
2023-05-27 00:47:42 +0800

    初始化

---
+++ src/views/Home/HomeMain.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home-main">
+    {{ bloodJSon[0] }}
+    <div class="main-body">
+    <div v-for="(item, index) in store.bloodJSon" key="item.id">
+      <MainCell :blood-obj="item" />
+    </div>
+  </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {
+  ref,
+  watch,
+  defineProps,
+  defineEmits,
+  onActivated,
+  computed,
+  nextTick,
+  watchEffect,
+  toRefs,
+} from "vue";
+import MainCell from "./MainCell.vue";
+import {useBlood} from "../../store/index.js";
+let store = useBlood()
+const props = defineProps({
+  bloodJSon: { type: Array, required: true, default: () => [] },
+});
+
+console.log();
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.main-body {
+  display: grid;
+  grid-template-columns: repeat(5, 20%);
+  grid-gap: 20px;
+
+}
+</style>
---
+++ src/views/Home/MainCell.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="main-cell" @click="toEdit">
+    <div class="block">
+      <el-avatar shape="square" :size="100" fit="cover" :src="bloodObj.image" />
+      <span class="title">{{ bloodObj.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {
+  ref,
+  watch,
+  defineProps,
+  defineEmits,
+  onActivated,
+  computed,
+  nextTick,
+  watchEffect,
+  toRefs,
+} from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const props = defineProps({
+  bloodObj: { type: Object, required: true, default: () => [] },
+});
+const { bloodObj } = toRefs(props);
+const toEdit = () => {
+  router.push({ path: `/edit/${bloodObj.value.id}`});
+};
+
+console.log();
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.main-cell {
+  display: flex;
+  text-align: center;
+  justify-content: space-between;
+}
+.main-cell .block {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 0;
+}
+
+.main-cell .title {
+  margin-bottom: 10px;
+  font-size: 14px;
+  color: var(--el-text-color-secondary);
+}
+</style>
